045fed2cdb553ae48fce65ec215fd9ed02643c9f
Salomon Chambi <salomonchambi@gmail.com>
2024-01-29 09:00:00 -0500

    feat: add css and images

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,12 +1,15 @@
+import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
+let p0 = $('<div id="logo"></div>');
 let p1 = $('<p>Holberton Dashboard</p>');
 let p2 = $('<p>Dashboard data for the students</p>');
-let p3 = $('<button>Click here to get started</button>');
+let p3 = $('<button><span>Click here to get started</span></button>');
 let p4 = $('<p id="count"></p>');
 let p5 = $('<p>Copyright - Holberton School</p>');
 
+$('body').append(p0);
 $('body').append(p1);
 $('body').append(p2);
 $('body').append(p3);

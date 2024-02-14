d2dbb6318ce01ad959e2c97bca81ece663d3b111
Salomon Chambi <salomonchambi@gmail.com>
2024-02-14 09:00:00 -0500

    feat: create a function to return urgent message

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -6,7 +6,12 @@ function getFooterCopy(isIndex) {
   return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 }
 
+function getLatestNotification() {
+  return {__html: '<strong>Urgent requirement</strong> - complete by EOD'};
+}
+
 export {
   getFullYear,
-  getFooterCopy
+  getFooterCopy,
+  getLatestNotification,
 };
